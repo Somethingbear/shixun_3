cee8a863b1eb838aca8890d3c7e1f9a6f3040e26
lilei <289670502@qq.com>
2018-01-21 10:35:17 +0800

    'opt'

--- src/app.js
+++ src/app.js
@@ -4,6 +4,48 @@
 
 import './scss/index.scss';
 
-import $ from 'jquery';
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
 
-$('#root').html(require('./demo.html'))
+import {
+    HashRouter as Router,
+    Route,
+} from 'react-router-dom';
+
+function About() {
+    return (<h2> About </h2>)
+}
+
+class Home extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            num: 10
+        }
+    }
+
+
+    render() {
+        return [
+            <h1>Home {this.state.num}</h1>,
+            <button onClick={() => {
+                this.setState({
+                    num: this.state.num + 10
+                });
+
+            }}>按钮</button>
+        ]
+    }
+}
+
+render(
+    <Router>
+        <div>
+            <Route path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </div>
+    </Router>, document.getElementById('shixun')
+);
