4efbae25b018678abe56e6b09420c0d34660332e
lilei <289670502@qq.com>
2018-01-28 11:39:59 +0800

    ''opt

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -3,15 +3,11 @@
  */
 
 import React from 'react';
-import {connect} from 'react-redux';
 
 import Header from './common/Header';
 import Siderbar from './common/Siderbar';
 import Footer from './common/Footer';
 
-@connect(
-    state => ({user: state.user, menu: state.menu})
-)
 export default class extends React.Component {
     render() {
         return [
