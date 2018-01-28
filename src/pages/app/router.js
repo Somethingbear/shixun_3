29a4c3289dace3b7e89744c27dfb74a6af2a6d7a
lilei <289670502@qq.com>
2018-01-28 12:00:26 +0800

    'opt'

--- src/pages/app/router.js
+++ src/pages/app/router.js
@@ -7,12 +7,14 @@ import {Route, Switch} from 'react-router-dom';
 
 import Workspace from './workspace';
 import User from './user';
+import Error from './error';
 
 export default function ({match}) {
     return (
         <Switch>
             <Route path={`${match.url}/workspace`} component={Workspace}/>
             <Route path={`${match.url}/user`} component={User}/>
+            <Route component={Error}/>
         </Switch>
     )
 }
